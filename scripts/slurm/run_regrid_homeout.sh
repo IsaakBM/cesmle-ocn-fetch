e145897bb29f6143877de9706b54a5a07a7160b1
IsaakBM <ibrito@conservation.org>
2026-02-03 15:09:59 -0800

    Toggle HIST SALT/UVEL job submissions
    In scripts/slurm/run_regrid_homeout.sh, comment out the historical SALT job submission and enable the UVEL submission instead. This switches which variable's historical regrid job is submitted in the HIST block; no other changes were made.

--- scripts/slurm/run_regrid_homeout.sh
+++ scripts/slurm/run_regrid_homeout.sh
@@ -32,8 +32,8 @@ jid=""
 
 # --- HISTORICAL ---
 #jid=$(submit_dep hist O2   "$jid")
-jid=$(submit_dep hist SALT "$jid")
-#jid=$(submit_dep hist UVEL "$jid")
+#jid=$(submit_dep hist SALT "$jid")
+jid=$(submit_dep hist UVEL "$jid")
 #jid=$(submit_dep hist TEMP "$jid")
 
 # --- RCP85 ---

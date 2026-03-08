7a00bd1cf860473ea0e8151fd7fb7c0d51c1fd80
IsaakBM <ibrito@conservation.org>
2026-03-08 14:01:08 -0700

    Update run_glorys_monthly_0p05.sh

--- scripts/slurm/run_glorys_monthly_0p05.sh
+++ scripts/slurm/run_glorys_monthly_0p05.sh
@@ -21,15 +21,15 @@ VARS=(
 )
 
 YEARS=(
-  2006
-  2007
-  2008
-  2009
+  #2006
+  #2007
+  #2008
+  #2009
   2010
-  2011
+  #2011
   2012
   2013
-  2014
+  #2014
 )
 
 mkdir -p /home/sandbox-sparc/cesmle-ocn-fetch/logs
